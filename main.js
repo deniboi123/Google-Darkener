1986db88dadfd138ecd73b59f8478f9273a5e24e
deniboi123 <58114191+deniboi123@users.noreply.github.com>
2021-06-27 08:59:34 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,3 @@
+// Copy this into your console menu, yes this is not malware in any way!
+
+document.getElementsByTagName('div')[0].style.background = '#18191A'; document.getElementsByTagName('a')[0].style.color = "white"; document.getElementsByTagName('a')[1].style.color = "white"; document.getElementsByTagName('a')[3].style.color = "white"; document.getElementsByClassName('gb_f')[0].style.color = "white";
